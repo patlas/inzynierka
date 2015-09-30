df950da0f5274d129fb37203736755113dbabc9f
patlas <plas@student.agh.edu.pl>
2015-09-30 20:00:03 +0200

    main.cpp to test g++ on beagle

--- beagle/cpp/server/test/main.cpp
+++ beagle/cpp/server/test/main.cpp
@@ -7,15 +7,40 @@
 #include <iostream>
 #include <thread>
 #include <unistd.h>
-#include "../tcp/TCPCommunication.h"
+//#include "../tcp/TCPCommunication.h"
+//#include "../tcp/Messanger.h"
+#include <mutex>
+#include <queue>
+#include <string>
+#include <stdint.h>
 
 using namespace std;
 
-void trd(){
+mutex tMutex, rMutex;
+queue<QueueStruct_t> tQueue, rQueue;
+queue<string> tq,rq;
 
-	for(int a=0; a<5;a++)
-	{
-		cout<<"Wątek policzyl do: "<<a<<endl;
+
+
+void trd(){
+int index = 0;
+	while(1){
+		if(tMutex.try_lock()){
+			string b = "trd";
+			tq.push(b);
+			tMutex.unlock();
+			cout<<"Wątek wstawił do kolejki "<<index<<endl;
+		}
+
+		if(rMutex.try_lock()){
+			if(!rq.empty()){
+				cout<<index<<": Wątek odczytał z kolejki: "<<rq.front()<<endl;
+				rq.pop();
+			}
+			rMutex.unlock();
+		}
+
+		index++;
 		sleep(1);
 	}
 }
@@ -23,18 +48,35 @@ void trd(){
 
 int main(void){
 
-	//thread t(trd);
-	TCPCommunication *a = new TCPCommunication(INADDR_ANY,12345);
-	/*for(int a = 0; a<3;a++)
-	{
-		cout<<"Glowny program policzyl do:"<<a<<endl;
-		sleep(2);
-	}*/
 
-	if(a->startServer() == NO_ERROR) cout<<"Connection has been established"<<endl;
-	uint8_t buff[] = "PATRYK";
-	a->sendData(buff);
-	while(1);
+	thread t(trd);
+	int index=0;
+	//TCPCommunication *a = new TCPCommunication(INADDR_ANY,12345);
+	while(1)
+	{
+		if(rMutex.try_lock()){
+			string b = "main";
+			rq.push(b);
+			rMutex.unlock();
+			cout<<"Main wstawił do kolejki "<<index<<endl;
+		}
+
+		if(tMutex.try_lock()){
+			if(!tq.empty()){
+				cout<<index<<": Main odczytał z kolejki: "<<tq.front()<<endl;
+				tq.pop();
+			}
+			tMutex.unlock();
+		}
+		index++;
+		sleep(2);
+	}
+//	tMutex.lock();
+//	if(a->startServer() == NO_ERROR) cout<<"Connection has been established"<<endl;
+//	uint8_t buff[] = "PATRYK";
+//	a->sendData(buff);
+//	while(1);
+
 
 
 
